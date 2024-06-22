b5624e3c7d4eeeeb8d9d538093fcc433a5d2747e
Ko Ko Maung <kokomaung@bimgoc.com>
2024-06-22 18:47:35 +0630

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/hsu_bakery/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hsu_bakery
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screens/nav.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class NavScreen extends StatefulWidget {
+  const NavScreen({super.key});
+
+  @override
+  State<NavScreen> createState() => _NavScreenState();
+}
+
+class _NavScreenState extends State<NavScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
